gustavcorpas/spyfall

commit 6fb55910e56d3c6a6fd250b304dc112270849c87

Add PWA capability

index.html
@@ -7,6 +7,8 @@
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="script.js"></script>
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#333333">
   </head>
   <body>
 

sw.js
@@ -1,0 +1,49 @@
+"use strict";
+
+//WARNING: The currentCache ID has to be changed every time the app is updated, otherwise existing users will not recieve the updates
+const currentCache = "cache-20200223";
+const urlsToCache = [
+	"/manifest.json",
+	"/",
+	"/style.css",
+	"https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
+	"/script.js",
+	"/favicon.svg",
+	"/favicon.png",
+	"/Hero/HeroLight-Regular.otf"
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(currentCache).then((cache) => {
+			return cache.addAll(urlsToCache);
+		}).catch((e) => {console.error("SW failed to install: could not load cache " + currentCache + ": " + e);})
+	);
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if(currentCache.indexOf(cacheName) === -1){
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		}).catch((e) => {console.error("SW failed to activate: could not remove legacy cache: " + e);})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(
+			event.request
+		).then((response) => {
+			if(response){
+				return response;
+			}
+			return fetch(event.request);
+		}).catch((e) => {console.error("SW failed to fetch resource: " + e);})
+	);
+});

script.js
@@ -1,3 +1,9 @@
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js");
+}
+
+
 var startGameTime = new Date();
 let currentTime = new Date();
 var gameLength = 10;

commit 1cc6f60d4e333ac6ad85f98133b40b5aeab335bb

Fix relative paths regression in 6fb5591

index.html
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="icon" href="./favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="favicon.svg" type="image/svg+xml">
     <title>Spyfall</title>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js"></script>
     <script src="script.js"></script>

sw.js
@@ -3,14 +3,14 @@
 //WARNING: The currentCache ID has to be changed every time the app is updated, otherwise existing users will not recieve the updates
 const currentCache = "cache-20200223";
 const urlsToCache = [
-	"/manifest.json",
-	"/",
-	"/style.css",
+	"manifest.json",
+	"./",
+	"style.css",
 	"https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
-	"/script.js",
-	"/favicon.svg",
-	"/favicon.png",
-	"/Hero/HeroLight-Regular.otf"
+	"script.js",
+	"favicon.svg",
+	"favicon.png",
+	"Hero/HeroLight-Regular.otf"
 ];
 
 self.addEventListener("install", (event) => {